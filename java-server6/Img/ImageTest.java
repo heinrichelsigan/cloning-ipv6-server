c008276a1001aa33881d1bbb1d54e67339820e5b
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2024-12-12 13:16:17 +0100

    2024-12-12 simple image display im java

---
+++ java-server6/Img/ImageTest.java
@@ -0,0 +1,35 @@
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class ImageTest {
+
+    public static void main(String avg[]) throws IOException
+    {
+        ImageTest imgTest = new ImageTest();
+    }
+
+    public ImageTest() throws IOException
+    {
+        BufferedImage img=ImageIO.read(new File("Screenshot 2024-12-09 040345.png"));
+        ImageIcon icon=new ImageIcon(img);
+        JFrame frame=new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(200,300);
+        JLabel lbl=new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
